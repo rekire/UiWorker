265a78353006aec681769d3486453c423919e23f
rekire <git@reki.re>
2014-02-06 14:29:53 +0100

    First maven release

--- build-maven.bat
+++ build-maven.bat
@@ -1,4 +1,4 @@
 @echo off
 call ..\maven-setup.bat
-package source:jar javadoc:jar gpg:sign repository:bundle-create
+mvn package source:jar javadoc:jar gpg:sign repository:bundle-create
 pause > nul
